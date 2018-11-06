7a289791741e76591dec5bfc5527b91ad7a2291b
Hentioe <me@bluerain.io>
2018-11-07 06:37:18 +0800

    Implement the irirserver module

--- ports/irirserver/src/main.rs
+++ ports/irirserver/src/main.rs
@@ -1,3 +1,72 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate actix_web;
+extern crate libcore;
+extern crate libresizer;
+
+use libcore::errors::*;
+use actix_web::{fs, server, HttpRequest, App};
+use libresizer::{ImageOption, ImageInfo};
+use std::path::PathBuf;
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref IOPTS: ImageOption = ImageOption::new("./originals", "./outputs");
+    static ref Cache: Mutex<HashMap<u64, u64>> = Mutex::new(HashMap::new());
+}
+
+fn display_img(req: &HttpRequest) -> Result<fs::NamedFile> {
+    let params = req.match_info();
+    let name = params.get("name").ok_or(err_msg("Missing name parameter"))?;
+    let format = params.get("format").ok_or(err_msg("Missing format parameter"))?;
+    let query = req.query();
+    let width = if let Some(w) = query.get("w") {
+        let w: u32 = w.parse()?;
+        if w > 1280 {
+            return Err(err_msg("Width out of range"));
+        }
+        Some(w)
+    } else {
+        None
+    };
+    let height = if let Some(h) = query.get("h") {
+        let h: u32 = h.parse()?;
+        if h > 1280 {
+            return Err(err_msg("Height out of range"));
+        }
+        Some(h)
+    } else {
+        None
+    };
+    let img_info = ImageInfo::new(name, format, width, height);
+    let info_hash = img_info.to_hash();
+    let mut cache = Cache.lock().unwrap();
+    // In cache
+    if cache.contains_key(&info_hash)  {
+        let hash= cache.get(&info_hash).ok_or(err_msg("Cache temporary error"))?;
+        let mut opath = PathBuf::from(&IOPTS.output_dir());
+        opath.push(hash.to_string());
+        opath.set_extension(format);
+        Ok(fs::NamedFile::open(opath.to_str().ok_or(err_msg("No output file found"))?)?)
+    } else { // Resize & Add cache
+        let hash = libresizer::resize(&IOPTS, &img_info)?;
+        let mut opath = PathBuf::from(&IOPTS.output_dir());
+        opath.push(hash.to_string());
+        opath.set_extension(format);
+        let nf = fs::NamedFile::open(opath.to_str().ok_or(err_msg("No output file found"))?)?;
+        cache.insert(img_info.to_hash(), hash);
+        Ok(nf)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let apps = || vec![
+        App::new()
+            .prefix("/display")
+            .resource("/", |r| r.f(|_req| "/{file_name} access image"))
+            .resource("/{name}.{format}", |r| r.f(display_img))
+            .finish()
+    ];
+    server::new(apps).bind("127.0.0.1:8080").unwrap().run();
 }
--- ports/libresizer/src/lib.rs
+++ ports/libresizer/src/lib.rs
@@ -4,8 +4,8 @@ extern crate libcore;
 use libcore::errors::*;
 use image::FilterType;
 use std::path::PathBuf;
-use std::hash::Hasher;
 use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 #[derive(Debug, Clone)]
 pub struct ImageOption {
@@ -14,31 +14,61 @@ pub struct ImageOption {
 }
 
 impl ImageOption {
-    fn new(input: &str, output: &str) -> ImageOption {
+    pub fn new(input: &str, output: &str) -> ImageOption {
         ImageOption { input_dir: input.to_string(), output_dir: output.to_string() }
+    }
+
+    pub fn input_dir(&self) -> &str {
+        self.input_dir.as_str()
+    }
+    pub fn output_dir(&self) -> &str {
+        self.output_dir.as_str()
     }
 }
 
-pub fn resize(opts: &ImageOption, fname: &str, nwidth: Option<u32>, nheight: Option<u32>) -> Result<u64> {
-    let width = if let Some(w) = nwidth {
+#[derive(Debug, Clone, Hash)]
+pub struct ImageInfo {
+    name: String,
+    format: String,
+    width: Option<u32>,
+    height: Option<u32>,
+}
+
+impl ImageInfo {
+    pub fn new(name: &str, format: &str, width: Option<u32>, height: Option<u32>) -> ImageInfo {
+        ImageInfo { name: name.to_string(), format: format.to_string(), width, height }
+    }
+
+    pub fn to_hash(&self) -> u64 {
+        let mut s = DefaultHasher::new();
+        self.hash(&mut s);
+        s.finish()
+    }
+
+    pub fn fname(&self) -> String {
+        format!("{}.{}", self.name, self.format)
+    }
+}
+
+pub fn resize(opts: &ImageOption, img_info: &ImageInfo) -> Result<u64> {
+    let width = if let Some(w) = img_info.width {
         w
     } else {
         std::u32::MAX
     };
-    let height = if let Some(h) = nheight {
+    let height = if let Some(h) = img_info.height {
         h
     } else {
         std::u32::MAX
     };
-    let mut fpath = PathBuf::from(&opts.input_dir);
-    fpath.push(fname);
+    let mut fpath = PathBuf::from(&opts.input_dir());
+    fpath.push(img_info.fname());
     let img = image::open(&fpath)?;
-    println!("{}", fpath.to_str().unwrap());
     let resized = img.resize(width, height, FilterType::Gaussian);
     let mut hasher = DefaultHasher::new();
     hasher.write(&resized.raw_pixels());
     let hash = hasher.finish();
-    let mut opath = PathBuf::from(&opts.output_dir);
+    let mut opath = PathBuf::from(&opts.output_dir());
     opath.push(hash.to_string());
     opath.set_extension(&fpath.extension().ok_or(err_msg("Did not get the extension of the input file"))?);
     resized.save(opath)?;
